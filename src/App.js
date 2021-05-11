24edefd7227c1fb0d63d75771b14fdece5571642
An88ton <an88ton@gmail.com>
2021-05-11 13:32:14 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import Car from "./Car/Car";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2019},
+            {name: 'Audi', year: 2021},
+            {name: 'Tesla', year: 2021}
+        ],
+        pageTitle: 'Cars',
+        showCars: false
+    }
+
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    changeTitleHandler = pageTitle => {
+        this.setState({pageTitle})
+    }
+
+    handleInput = (event) => {
+        this.setState({
+            pageTitle: event.target.value
+        });
+    }
+
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+        //const cars = this.state.cars
+
+        return (
+            <div style={divStyle}>
+                <h1 style={{color: "green"}}>{this.state.pageTitle}</h1>
+
+                <input type="text" onChange={this.handleInput}/>
+                <br />
+                <br />
+                <button
+                    onClick={this.toggleCarsHandler}
+                >Toggle cars
+                </button>
+
+                {this.state.showCars
+                    ? this.state.cars.map((car, index) => {
+                        return (
+                            <Car
+                                key={index}
+                                name={car.name}
+                                year={car.year}
+                                onChangeTitle={this.changeTitleHandler.bind(this, car.name)}
+                            />
+                        )
+                    })
+                    :
+                        null}
+                {/*<Car*/}
+                {/*    name={cars[0].name}*/}
+                {/*    year={cars[0].year}*/}
+                {/*    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}*/}
+                {/*/>*/}
+                {/*<Car*/}
+                {/*    name={cars[1].name}*/}
+                {/*    year={cars[1].year}*/}
+                {/*    onChangeTitle={this.changeTitleHandler.bind(this, cars[1].name)}*/}
+                {/*>*/}
+                {/*    <p style={{color: "red"}}>Color</p>*/}
+                {/*</Car>*/}
+                {/*<Car*/}
+                {/*    name={cars[2].name}*/}
+                {/*    year={cars[2].year}*/}
+                {/*    onChangeTitle={ () => this.changeTitleHandler(cars[2].name)}*/}
+                {/*/>*/}
+            </div>
+        );
+    }
 }
 
 export default App;
